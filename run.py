ba65962aab0a300d96ae4fab21e7aa86d6054b40
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-05-14 00:10:06 +0000

    collect sales for the last five markets in lists of columns

--- run.py
+++ run.py
@@ -82,6 +82,20 @@ def update_worksheet(data, worksheet):
     print(f"{worksheet} worksheet updated succesfully!\n") 
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting the last 
+    5 entries for each sandwich and returns the data as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -93,4 +107,5 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwich Data Automation")
-main()
+# main()
+sales_columns = get_last_5_entries_sales()
